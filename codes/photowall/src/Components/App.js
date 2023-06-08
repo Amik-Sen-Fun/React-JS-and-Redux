d609d22cab3618e5aa3be923986f4cfffa619045
Amik-Sen-Fun <bongleesen@gmail.com>
2023-06-08 18:59:00 +0530

    Started using states

--- codes/photowall/src/Components/App.js
+++ codes/photowall/src/Components/App.js
@@ -1,38 +1,46 @@
 import '../Stylesheets/App.css';
 import Photowall from './Photowall';
+import React, {Component} from 'react';
 
-function App() {
-  const posts = [
-    {
-        name: 'Steve Jobs', 
-        url: 'https://drive.google.com/uc?export=view&id=1NY0vrn7GzlYHlKk7Rynlz21ee1Q_zGYc'
-    },
-    {
-        name: 'Amik Sen',
-        url: 'https://drive.google.com/uc?export=view&id=1023md3ZN9dy--aeSgTKXWTsHFp2u8Dgz'
-    },
-    {
-        name: 'Mridul Newar',
-        url: 'https://drive.google.com/uc?export=view&id=1Nd3vxasz_MJHV23ocZOTaXMq6cbhTRR8'
-    },
-    {
-        name: 'Dushyant Trivedi',
-        url: 'https://drive.google.com/uc?export=view&id=18wsq-iOKzGetWMIfoVPYdpomgVAJUDyN'
-    },
-    {
-        name: 'Bill Gates',
-        url: 'https://drive.google.com/uc?export=view&id=18six-cV3eyXTrSdPkjE02fAWId5ns5f8'
-    },
-];
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      posts : [
+          {
+              name: 'Steve Jobs', 
+              url: 'https://drive.google.com/uc?export=view&id=1NY0vrn7GzlYHlKk7Rynlz21ee1Q_zGYc'
+          },
+          {
+              name: 'Amik Sen',
+              url: 'https://drive.google.com/uc?export=view&id=1023md3ZN9dy--aeSgTKXWTsHFp2u8Dgz'
+          },
+          {
+              name: 'Mridul Newar',
+              url: 'https://drive.google.com/uc?export=view&id=1Nd3vxasz_MJHV23ocZOTaXMq6cbhTRR8'
+          },
+          {
+              name: 'Dushyant Trivedi',
+              url: 'https://drive.google.com/uc?export=view&id=18wsq-iOKzGetWMIfoVPYdpomgVAJUDyN'
+          },
+          {
+              name: 'Bill Gates',
+              url: 'https://drive.google.com/uc?export=view&id=18six-cV3eyXTrSdPkjE02fAWId5ns5f8'
+          },
+      ]
+    }
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Photowall</h1>
-        <Photowall posts ={posts}/>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Photowall</h1>
+          <Photowall posts ={this.state.posts}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
